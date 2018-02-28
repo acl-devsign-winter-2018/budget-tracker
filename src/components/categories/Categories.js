0762c8c4c87a9b143c5b3f6d2d1952e07271e1d2
mewarren <holla@mewarren.com>
2018-02-28 11:25:46 -0800

    add categories component

---
+++ src/components/categories/Categories.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addCat } from './actions';
+import Category from './Category';
+import CategoryForm from './CategoryForm';
+
+class Categories extends Component {
+
+  render() {
+    const { addCat, categories } = this.props;
+    return (
+      <div>
+        <CategoryForm onEdit={addCat}/>
+        <ul>
+          {categories.map(category => <Category key={category.id} {...category}/>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  state => ({ categories: state }),
+  { addCat }
+)(Categories);
