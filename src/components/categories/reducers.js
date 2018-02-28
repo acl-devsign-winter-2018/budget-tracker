da50d15741cec66cf1322cdf22016fd8352044ab
Jennifer Elton <justjenny40@hotmail.com>
2018-02-28 09:52:58 -0800

    added reducers to categories

---
+++ src/components/categories/reducers.js
@@ -0,0 +1,28 @@
+export const CATEGORY_CREATE = 'CATEGORY_CREATE';
+export const CATEGORY_UPDATE = 'CATEGORY_UPDATE';
+export const CATEGORY_DESTROY = 'CATEGORY_DESTROY';
+
+export function category(state = [], { type, payload }) {
+  switch(type) {
+    case CATEGORY_CREATE:
+      return [
+        ...state,
+        payload
+      ];
+
+    case CATEGORY_UPDATE: {
+      const index = state.findIndex(n => n.id === payload.id);
+      return [
+        ...state.slice(0, index),
+        { ...state[index], ...payload },
+        ...state.slice(index + 1)
+      ];
+    }
+
+    case CATEGORY_DESTROY: 
+      return state.filter(n => n.id !== payload.id);
+
+    default:
+      return state;
+  }
+}
