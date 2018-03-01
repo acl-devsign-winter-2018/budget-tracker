5a06e0c6f05921eb0d64b833dceed3e7b5e99005
Christina Mills <chrimills@gmail.com>
2018-02-28 20:36:26 -0800

    failing test

--- src/components/category/actions.test.js
+++ src/components/category/actions.test.js
@@ -4,9 +4,20 @@ import { addCategory, updateCategory, removeCategory } from './actions';
 it('creates an add action', () => {
   const { type, payload } = addCategory({ category: 'Beer' });
 
-  expect(type).toBe(CATEGORY_ADD)
+  expect(type).toBe(CATEGORY_ADD);;
   const { category, id, timestamp } = payload;
   expect(category).toBe('Beer');
   expect(id).toBeTruthy();
   expect(timestamp).toBeTruthy();
+});
+
+it('creates an update action', () => {
+  const action = updateCategory({ id: 123, category: 'vacation' });
+  expect(action).toEqual({
+    type: CATEGORY_UPDATE,
+    payload: {
+      id: 123,
+      category: 'vacation'
+    }
+  });
 });
